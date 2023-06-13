8e5460671daa30c8311f21b1873b2985688969f4
Jacob <jmayereup@gmail.com>
2023-06-13 20:05:46 +0700

    added translate attribute

---
+++ src/components/read-lines/read-lines.tsx
@@ -0,0 +1,49 @@
+import { Component, Element, Prop, State, h } from '@stencil/core';
+
+@Component({
+    tag: 'tj-read-lines',
+    styleUrl: './read-lines.css',
+    shadow: true
+})
+
+export class ReadingTool {
+
+@Element() el: HTMLElement;
+
+@Prop() setLang: string = 'en-CA';
+@Prop() sourceID: string =  '';
+
+@State() isPlaying = false;
+@State() lines: string[] = [];
+
+componentWillLoad() {
+    const sourceText = this.el.textContent;
+    console.log("will load", sourceText);
+    this.lines = this.prepareLines(sourceText);
+  
+}
+
+componentDidLoad() {
+    const shadowRoot = this.el.shadowRoot;
+    console.log('Component did load');
+    const translateItems = shadowRoot.querySelectorAll(".translate");
+    console.log(translateItems);
+    translateItems.forEach(item => item.setAttribute('translate', 'yes'));
+}
+
+
+prepareLines(text) {
+    let content: string = text.trim();
+    content = content.replace(/([.?!])\s*(?=[A-Z])/g, '$1\n');
+    return this.lines = content.split('\n');
+};
+
+render() {
+    return <ul>{this.lines.map(line => (<span><li>{line}</li><li class="translate">{line}</li></span>))}
+    </ul>;
+}
+
+
+}
+
+
